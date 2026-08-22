86d35687e25bb3ba22ac8190981f7afd7d2ddc36
abhijeetagnihotri123 <31515456+abhijeetagnihotri123@users.noreply.github.com>
2026-08-22 17:07:10 +0000

    Solution to avoid drift using wake until

--- adas_pipeline/src/control_loop.cpp
+++ adas_pipeline/src/control_loop.cpp
@@ -5,13 +5,12 @@ namespace sensor_data_consumer{
     void consumer_sensor(sensor_queue &queue , std::atomic<bool>&running){
 
         while(running.load()){
+            auto next_wake = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds(20);
             adas::SensorFrame *frame = queue.pop(sensor_data_consumer::time_out);
             if(frame != nullptr){
                 std::cout<<*(frame)<<std::endl;
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+            std::this_thread::sleep_until(next_wake);
         }
-
     }
-
 }
--- adas_pipeline/src/sensor_producers.cpp
+++ adas_pipeline/src/sensor_producers.cpp
@@ -7,43 +7,46 @@ namespace sensor_data_producers{
         int id = 0;
         while(running.load()){
             adas::SensorFrame *frame = new adas::SensorFrame;
+            auto next_wake = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds(100);
             frame->type = adas::SensorType::LiDar;
             frame->distance = 50.5;
             frame->relative_velocity = 121.9;
             frame->time_now = std::chrono::high_resolution_clock::now();
             frame->frame_id = id++; 
             queue.push(frame);
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_until(next_wake);
         }
-
     }
+
     void radar_producers(sensor_queue &queue , std::atomic<bool>&running){
 
         int id = 0;
         while(running.load()){
             adas::SensorFrame *frame = new adas::SensorFrame;
+            auto next_wake = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds(80);
             frame->type = adas::SensorType::Radar;
             frame->distance = 90.9;
             frame->relative_velocity = 119.1;
             frame->time_now = std::chrono::high_resolution_clock::now();
             frame->frame_id = id++;
             queue.push(frame);
-            std::this_thread::sleep_for(std::chrono::milliseconds(80));
+            std::this_thread::sleep_until(next_wake);
         }
     }
+
     void camera_producers(sensor_queue &queue , std::atomic<bool>&running){
 
         int id = 0;
         while(running.load()){
             adas::SensorFrame *frame = new adas::SensorFrame;
+            auto next_wake = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds(60);
             frame->type = adas::SensorType::Camera;
             frame->distance = 200.11;
             frame->relative_velocity = 44.5;
             frame->time_now = std::chrono::high_resolution_clock::now();
             frame->frame_id = id++;
             queue.push(frame);
-            std::this_thread::sleep_for(std::chrono::milliseconds(60));
+            std::this_thread::sleep_until(next_wake);
         }
     }
-
 }
